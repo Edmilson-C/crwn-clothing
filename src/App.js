e55841406da2a85b4cf43f0b8eacbc64b4c74ae0
edmilson-da-conceicao <edmilson.conceicao09@gmail.com>
2021-06-27 15:17:53 +0200

    feat: add homepage route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
 import Homepage from './pages/homepage/homepage.component';
 
 function App() {
   return (
-    <div> <Homepage /> </div>
+    <div> 
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+      </Switch>
+    </div>
   );
 }
 
