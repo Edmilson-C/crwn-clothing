a6203d3db198911d9a66546f9f04a81e18c1a9f7
edmilson-da-conceicao <edmilson.conceicao09@gmail.com>
2021-06-28 23:24:49 +0200

    feat: change function component to class component & add authStateChanged

--- src/App.js
+++ src/App.js
@@ -8,17 +8,41 @@ import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import LoginRegister from './pages/login-register/login-register.component';
 
-function App() {
-  return (
-    <div> 
-      <Header /> 
-      <Switch>
-        <Route exact path='/' component={Homepage} />
-        <Route exact path='/shop' component={ShopPage} />
-        <Route exact path='/signin' component={LoginRegister} /> 
-      </Switch>
-    </div>
-  );
+import { auth } from './firebase/firebase.utils';
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user })
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div> 
+        <Header currentUser={this.state.currentUser} /> 
+        <Switch>
+          <Route exact path='/' component={Homepage} />
+          <Route exact path='/shop' component={ShopPage} />
+          <Route exact path='/signin' component={LoginRegister} /> 
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
