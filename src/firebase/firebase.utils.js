022b73a4682fca64f13f5e31364b0f5ad3c6c19c
edmilson-da-conceicao <edmilson.conceicao09@gmail.com>
2021-06-28 22:53:11 +0200

    feat: add firebase

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+let config = {
+  apiKey: "AIzaSyA8KkjHJVM3LUTFtOWOWXwq4Vm4UKKmX18",
+  authDomain: "crwn-clothing-a28b2.firebaseapp.com",
+  projectId: "crwn-clothing-a28b2",
+  storageBucket: "crwn-clothing-a28b2.appspot.com",
+  messagingSenderId: "639041143924",
+  appId: "1:639041143924:web:d836fce98da105c6e17d21",
+  measurementId: "G-82FGJLNNLW"
+};
+// Initialize Firebase
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
