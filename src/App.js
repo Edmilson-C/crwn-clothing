27cd771cd75093e320259d510160cc99c3be73f1
edmilson-da-conceicao <edmilson.conceicao09@gmail.com>
2021-07-05 15:52:57 +0200

    feat: redirect to home if user is signed in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 
 import "./App.css";
@@ -47,12 +47,22 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={Homepage} />
           <Route exact path="/shop" component={ShopPage} />
-          <Route exact path="/signin" component={LoginRegister} />
+          <Route
+            exact
+            path="/signin"
+            render={() =>
+              this.props.setCurrentUser ? <Redirect to="/" /> : <LoginRegister />
+            }
+          />
         </Switch>
       </div>
     );
   }
 }
+
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser,
+});
 
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: (user) => dispatch(setCurrentUser(user)),
