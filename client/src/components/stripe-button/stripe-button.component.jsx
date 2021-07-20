b6a1d14c28d8357f58dd2f59829581356897b0a1
edmilson-da-conceicao <edmilson.conceicao09@gmail.com>
2021-07-20 02:21:37 +0200

    feat: make post request to backend & receive response/error

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
 import StripeCheckout from 'react-stripe-checkout'
+import axios from 'axios'
 
-import Logo from "../../assets/crown.svg";
+import Logo from '../../assets/crown.svg';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100
   const PUBLISHABLE_KEY = 'pk_test_51JE9xTHTzvjwUlJCjrzBfcjq11GBgxmPV7sfprYkaTt6qQM8Xa72EB31rXtvdJTv3scYh6XXNyhIZDPC6QS3yDQo00YFWQNhCk'
 
   function onToken(token) {
-    alert('Payment Successful')
+    axios({
+      url: 'payment',
+      method: 'POST',
+      data: {
+        amount: priceForStripe,
+        token: token
+      }
+    }).then(response => {
+      alert('Payment successful')
+    }).catch(error => {
+      alert('Error making payment')
+      console.log('Payment error', error);
+    })
   }
 
   return (
