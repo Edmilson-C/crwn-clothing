f74852af53af60f1f3646417cbdb67e7b2d69dc6
edmilson-da-conceicao <edmilson.conceicao09@gmail.com>
2021-07-21 19:12:06 +0200

    feat: add hidden & toggle hidden context

--- client/src/components/header/header.component.jsx
+++ client/src/components/header/header.component.jsx
@@ -1,53 +1,64 @@
-import React, { useContext } from "react";
-import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
+import React, { useContext, useState } from "react";
+// import { connect } from "react-redux";
+// import { createStructuredSelector } from "reselect";
 
 import CartIcon from "../cart-icon/cart-icon.component";
 import CartDropdown from "../cart-dropdown/cart-dropdown.component";
-import { HeaderContainer, LogoContainer, OptionsContainer, OptionLink } from "./header.styles";
+import {
+  HeaderContainer,
+  LogoContainer,
+  OptionsContainer,
+  OptionLink,
+} from "./header.styles";
 
 import CurrentUserContext from "../../contexts/current-user/current-user.context";
+import CartContext from "../../contexts/cart/cart.context";
 
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { auth } from "../../firebase/firebase.utils";
-import { selectCartHidden } from "../../redux/cart/cart.selectors";
+// import { selectCartHidden } from "../../redux/cart/cart.selectors";
 // import { selectCurrentUser } from "../../redux/user/user.selectors";
 
 import "./header.styles.scss";
 
-const Header = ({ hidden }) => { 
-  const currentUser = useContext(CurrentUserContext)
+const Header = () => {
+  const currentUser = useContext(CurrentUserContext);
+  const [hidden, setHidden] = useState(true);
+  const toggleHidden = () => setHidden(!hidden);
 
   return (
-  <HeaderContainer>
-    <LogoContainer to="/">
-      <Logo />
-    </LogoContainer>
-    <OptionsContainer>
-      <OptionLink to="/shop">
-        SHOP
-      </OptionLink>
-      <OptionLink to="/contact">
-        CONTACT
-      </OptionLink>
-      {currentUser ? (
-        <OptionLink as="div" onClick={() => auth.signOut()}>
-          SIGN OUT
-        </OptionLink>
-      ) : (
-        <OptionLink to="/signin">
-          SIGN IN
-        </OptionLink>
-      )}
-      <CartIcon />
-    </OptionsContainer>
-    {hidden ? null : <CartDropdown />}
-  </HeaderContainer>
-)}
+    <HeaderContainer>
+      <LogoContainer to="/">
+        <Logo />
+      </LogoContainer>
+      <OptionsContainer>
+        <OptionLink to="/shop">SHOP</OptionLink>
+        <OptionLink to="/contact">CONTACT</OptionLink>
+        {currentUser ? (
+          <OptionLink as="div" onClick={() => auth.signOut()}>
+            SIGN OUT
+          </OptionLink>
+        ) : (
+          <OptionLink to="/signin">SIGN IN</OptionLink>
+        )}
+        <CartContext.Provider
+          value={{
+            hidden,
+            toggleHidden,
+          }}>
+          <CartIcon />
+        </CartContext.Provider>
+      </OptionsContainer>
+      {hidden ? null : <CartDropdown />}
+    </HeaderContainer>
+  );
+};
 
-const mapStateToProps = createStructuredSelector({
-  // currentUser: selectCurrentUser,
-  hidden: selectCartHidden,
-});
+export default Header;
 
-export default connect(mapStateToProps)(Header);
+// const mapStateToProps = createStructuredSelector({
+//   currentUser: selectCurrentUser,
+//   hidden: selectCartHidden,
+// });
+
+// export default connect(mapStateToProps)(Header);
